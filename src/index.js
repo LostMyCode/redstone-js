b1b2205ab1842cf7855685374812c1734b62b072
Ryswick <supersonic@aespa.jp>
2023-02-07 02:04:05 +0900

    Webpack: Single entry point

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './components/App';
+import "./game";
 
 const rootEl = document.getElementById('root');
 
