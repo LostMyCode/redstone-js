b4cb025feb01c6afddc5a77e3e3d6e67e9847645
Ryswick <supersonic@aespa.jp>
2023-02-07 22:41:18 +0900

    Expandable map list

--- src/components/MapListPanel.js
+++ src/components/MapListPanel.js
@@ -4,7 +4,9 @@ import RedStone from "../../game/RedStone";
 class MapListPanel extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            expanded: false,
+        };
     }
 
     componentDidMount() {
@@ -26,24 +28,37 @@ class MapListPanel extends React.Component {
         RedStone.loadMap(rmdFileName);
     }
 
+    handleExpanderClick = (e) => {
+        const { expanded } = this.state;
+        this.setState({ expanded: !expanded });
+    }
+
     render() {
         const mapList = window.mapList;
+        const { expanded } = this.state;
 
         if (!mapList) return null;
 
         return (
-            <div className="map-list-panel" style={{ height: window.innerHeight * 0.8, overflowY: "scroll" }}>
-                {Object.keys(mapList).map(key => {
-                    return (
-                        <div
-                            key={`map-${mapList[key].fileName}`}
-                            className="map-list-item"
-                            onClick={(e) => this.handleItemClick(e, mapList[key].fileName)}
-                        >
-                            {mapList[key].name}
-                        </div>
-                    )
-                })}
+            <div className="map-list-container">
+                <div className="map-list-expander" onClick={this.handleExpanderClick}>{expanded ? "Close map list" : "Show map list"}</div>
+                <div className="map-list-panel" style={{
+                    height: expanded ? window.innerHeight * 0.8 : 0,
+                    overflowY: "scroll",
+                    opacity: expanded ? 1 : 0
+                }}>
+                    {Object.keys(mapList).map(key => {
+                        return (
+                            <div
+                                key={`map-${mapList[key].fileName}`}
+                                className="map-list-item"
+                                onClick={(e) => this.handleItemClick(e, mapList[key].fileName)}
+                            >
+                                {mapList[key].name}
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
         )
     }
