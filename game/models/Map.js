12bd74ea9ab1a8e62317703cad49015a1e3acaa8
Ryswick <supersonic@aespa.jp>
2023-02-07 22:17:35 +0900

    Mapset: Desert = 2, Heaven = 9, Augusta = 12, Bridgehead = 13, Gypsy = 18

--- game/models/Map.js
+++ game/models/Map.js
@@ -15,17 +15,22 @@ export const MapType = {
 export const Mapset = {
     Grassland: 0,
     Mountains: 1,
+    Desert: 2,
     Savana: 3,
     Cave: 4,
     Dungeon: 5,
     Tower: 6,
     Mine: 7,
     Hell: 8,
+    Heaven: 9,
     Brunenstig: 10,
     Bigaple: 11,
+    Augusta: 12,
+    Bridgehead: 13,
     Mountains_Village: 14,
     Arian: 15,
     FarmHouse: 17,
+    Gypsy: 18,
     Room: 19,
 }
 
