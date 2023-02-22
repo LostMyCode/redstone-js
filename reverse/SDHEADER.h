d9b265da01cb66461922b0ee613b3e7f196dc456
LostMyCode <63048878+LostMyCode@users.noreply.github.com>
2023-02-23 00:34:46 +0900

    `SDHEADER` exported from Bada

---
+++ reverse/SDHEADER.h
@@ -0,0 +1,12 @@
+// SDHEADER exported from Bada
+
+struct __declspec(align(4)) $F8463B97287BEE435BC1537A8DAD96E9 // size: 0x40
+{
+  int Size;                       // offset: 0x00, size: 0x04
+  char Reg[50];                   // offset: 0x04, size: 0x32
+  unsigned __int8 BPP;            // offset: 0x36, size: 0x01
+  unsigned __int16 ImageCount;    // offset: 0x38, size: 0x02
+  unsigned __int8 Alpha;          // offset: 0x3A, size: 0x01
+  unsigned __int8 bOutline;       // offset: 0x3B, size: 0x01
+  unsigned __int8 bShadow;        // offset: 0x3C, size: 0x01
+};
