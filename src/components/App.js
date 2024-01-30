7a3708c607f818af1b8edc468775b08d9539b1f5
LostMyCode <63048878+LostMyCode@users.noreply.github.com>
2024-01-30 22:56:29 +0900

    Add `Sonner` toast library

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Toaster } from 'sonner';
+
 import DisplayLog from './DisplayLog';
 import TopRightContent from './TopRightContent';
 import ModalProvider from './ModalProvider';
 
 export const App = () => (
     <ModalProvider>
+        <Toaster richColors position="top-left" expand={false} />
         <DisplayLog />
         <TopRightContent />
     </ModalProvider>
